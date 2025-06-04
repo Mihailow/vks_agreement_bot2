b0756635bb4dc7cfcbcf13f4bf5ccc3f9a1c48b8
Михаил <70744799+Mihailow@users.noreply.github.com>
2025-06-04 15:55:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,17 +13,11 @@ DB_NAME = "vks_main"
 DB_USER = "postgres"
 DB_PASS = "postgres"
 
-telegram_token = "6353026522:AAG00eY0ErQEKJwacUPkgjB85X_1d6w3zWs"
+telegram_token = ""
 is_testing = True
-# from_email = "mihailbramnik@yandex.ru"
-# from_email_password = "jqukfyyvtjzutkow"
-dest_email = "mike-bramnik@yandex.ru"
-
-# telegram_token = "7302558225:AAExKM-A4cex_vPIt_-O5f-nrfHieYnit_c"
-# is_testing = False
-from_email = "vks.olga@yandex.ru"
-from_email_password = "kznofsnmcnctgjye"
-# dest_email = "tender@vksproect.ru"
+dest_email = ""
+from_email = ""
+from_email_password = ""
 
 subject = "Согласование документов"
 
